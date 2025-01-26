c24bac23877d62607094c844531510a7eee83976
Влад is love <97906766+Art-Rock-Climber@users.noreply.github.com>
2025-01-26 15:08:03 +0500

    Лабораторная работа №1
    Создание скрипта PowerShell для переименования файлов по возрастанию даты их создания

---
+++ Lab1.ps1
@@ -0,0 +1,35 @@
+﻿param (
+    [string]$FolderPath
+)
+
+# Проверка на пустую строку
+if ([string]::IsNullOrWhiteSpace($FolderPath)) {
+    do {
+        $FolderPath = Read-Host "Укажите путь к папке с фотографиями"
+    } while ([string]::IsNullOrWhiteSpace($FolderPath))
+}
+
+# Проверка существования папки
+if (-not (Test-Path -Path $FolderPath)) {
+    Write-Error "Указанная папка не существует."
+    return
+}
+
+# Список всех файлов формата JPG в папке, упорядоченный по возрастанию времени создания
+$files = Get-ChildItem -Path $FolderPath -Filter *.jpg | Sort-Object -Property CreationTime
+
+# Первый проход: временное переименование файлов
+# (Если сразу переименовывать как надо, могут совпасть имена файлов)
+for ($i = 1; $i -le $files.Count; $i++) {
+    $file = $files[$i - 1]
+    Rename-Item -Path $file.FullName -NewName "$i.jpg"
+}
+
+# Второй проход: приведение имен к нужному формату
+for ($i = 1; $i -le $files.Count; $i++) {
+    $oldFileName = "$i.jpg"
+    $newFileName = "IMG_{0:D4}.jpg" -f $i
+    Rename-Item -Path "$FolderPath\$oldFileName" -NewName $newFileName
+}
+
+#C:\Users\banni\lab_1_mat\source
